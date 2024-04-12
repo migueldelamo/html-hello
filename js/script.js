105e2c32ef763bfcb9a6611ef92ad192f1c6d1f3
migueldelamo <126247768+migueldelamo@users.noreply.github.com>
2024-04-12 14:57:21 +0000

    add guide

---
+++ js/script.js
@@ -0,0 +1,14 @@
+// Consola del navegador. ¿Qué es un console.log?
+
+// var, let, const.
+
+// Tipos de datos
+
+// Operaciones y operaciones lógicas
+
+// Controlando el flujo de ejecución
+// Condicionales.
+
+// Arrays y bucles.
+
+// Funciones.
