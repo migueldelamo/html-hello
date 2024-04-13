14bc176391f1c7e1a2c98fbc5436f1a040de8027
migueldelamo <126247768+migueldelamo@users.noreply.github.com>
2024-04-13 12:48:56 +0000

    prueba

--- js/script.js
+++ js/script.js
@@ -1,14 +1,14 @@
 // Consola del navegador. ¿Qué es un console.log?
 
-// var, let, const.
+// Variables. var, let, const.
 
 // Tipos de datos
 
 // Operaciones y operaciones lógicas
 
 // Controlando el flujo de ejecución
-// Condicionales.
-
-// Arrays y bucles.
-
-// Funciones.
+// 1. If-else
+// 2. Switch
+// 3. Operadores ternarios
+// 4. Arrays y bucles.
+// 5. Funciones.
